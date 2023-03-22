87df5b9650ae70b9e126bbbb31dad9db6bc4662f
1Suro1 <timu.zaxarov@gmail.com>
2023-03-22 19:52:05 +0900

    add icons

--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -8,7 +8,10 @@ export { default as ArrowDown } from "./icons/ArrowDown.svg";
 export { default as ArrowUp } from "./icons/ArrowUp.svg";
 export { default as Chat } from "./icons/Chat.svg";
 export { default as Close } from "./icons/Close.svg";
-export { default as CloseIcon } from "./icons/Close icon.svg"
-export { default as Frame } from "./icons/Frame.svg"
-
-
+export { default as CloseIcon } from "./icons/Close icon.svg";
+export { default as Frame } from "./icons/Frame.svg";
+export { default as Mail } from "./icons/Heart.svg";
+export { default as Phone } from "./icons/Phone.svg";
+export { default as Symbol } from "./icons/Symbol.svg";
+export { default as Tools } from "./icons/Tools.svg";
+export { default as Widget } from "./icons/Widget.svg";
